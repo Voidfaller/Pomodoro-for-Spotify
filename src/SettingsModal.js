e5175aa0b350519d4cc6290073b1ccf137c94560
Voidfaller <giovannidegroot@gmail.com>
2026-01-31 19:28:48 +0100

    feat: Add audio notification feature with settings option

--- src/SettingsModal.js
+++ src/SettingsModal.js
@@ -1,5 +1,5 @@
 // Settings modal component
-function SettingsModal({ isOpen, onClose, continuousTimer, onContinuousTimerChange }) {
+function SettingsModal({ isOpen, onClose, continuousTimer, onContinuousTimerChange, audioNotifications, onAudioNotificationsChange }) {
     if (!isOpen) return null;
     
     return React.createElement(
@@ -24,6 +24,22 @@ function SettingsModal({ isOpen, onClose, continuousTimer, onContinuousTimerChan
                     React.createElement("span", { className: "gp-label-text" }, "Continuous Timer")
                 ),
                 React.createElement("p", { className: "gp-setting-description" }, "Automatically start breaks and work sessions without pausing")
+            ),
+            React.createElement(
+                "div",
+                { className: "gp-settings-item" },
+                React.createElement(
+                    "label",
+                    { className: "gp-setting-label" },
+                    React.createElement("input", {
+                        type: "checkbox",
+                        checked: audioNotifications,
+                        onChange: (e) => onAudioNotificationsChange(e.target.checked),
+                        className: "gp-checkbox"
+                    }),
+                    React.createElement("span", { className: "gp-label-text" }, "Audio Notifications")
+                ),
+                React.createElement("p", { className: "gp-setting-description" }, "Play a subtle chime sound when timer completes")
             ),
             React.createElement("button", { onClick: onClose, className: "gp-modal-close" }, "Close")
         )
